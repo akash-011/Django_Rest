fa4584595f6c0fa1c4f6735ce3dfe874eb1d8063
Akash <akashbaga011@gmail.com>
2017-11-17 15:28:11 +0300

    [feature]Added Serializers

--- bucketlistapi/api/models.py
+++ bucketlistapi/api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Bucketlist(models.model):
+class Bucketlist(models.Model):
 
-    name = model.CharField(max_length='255', blank=False, unique=True)
-    date_created = model.DateTimeField(auto_now_add=True)
-    date_modified = model.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return"{}".format(self.name)
--- bucketlistapi/api/tests.py
+++ bucketlistapi/api/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from .models import bucketlist
-
+from .models import Bucketlist
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 class ModelTestCase(TestCase):
     "Tests for Bucketlist Model"
@@ -9,10 +11,23 @@ class ModelTestCase(TestCase):
         self.bucketlist_name = "Go to Old Trafford"
         self.bucketlist = Bucketlist(name=self.bucketlist_name)
 
-    def test_create_bucketlist(self):
+    def test_create_bucketlist_model(self):
         old_count = Bucketlist.objects.count()
-        self.Bucketlist.save()
+        self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count,new_count)
 
-    
+class ViewTestCase(TestCase):
+
+
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Score a goal'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json"
+        )
+
+    def test_api_create_bucketlist(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
